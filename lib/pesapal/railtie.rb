5813d6e1a2c47da4d378a412eb77f3be780b4a7c
King'ori Maina <j@kingori.co>
2014-01-31 03:01:31 +0300

    Set default values if YAML file is missing

--- lib/pesapal/railtie.rb
+++ lib/pesapal/railtie.rb
@@ -9,10 +9,15 @@ module Pesapal
         begin
           config.pesapal_credentials = YAML::load(IO.read(path_to_yaml))[Rails.env]
         rescue Errno::ENOENT
-          logger.info('YAML configuration file couldn\'t be found. Using defaults.'); return
+          logger.info('YAML configuration file couldn\'t be found.'); return
         rescue Psych::SyntaxError
           logger.info('YAML configuration file contains invalid syntax. Will use using defaults.'); return
         end
+      else
+        config.pesapal_credentials = { :callback_url => 'http://0.0.0.0:3000/pesapal/callback',
+                                       :consumer_key => '<YOUR_CONSUMER_KEY>',
+                                       :consumer_secret => '<YOUR_CONSUMER_SECRET>'
+                                      }
       end
     end
   end
