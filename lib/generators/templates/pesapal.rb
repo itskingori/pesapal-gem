2e09bb05a0e900a02fbf88c31a7fbc5e03dbcbd6
mogetutu <imogetutu@gmail.com>
2014-01-31 20:30:11 +0300

    don't know what happened here

--- lib/generators/templates/pesapal.rb
+++ lib/generators/templates/pesapal.rb
@@ -1,2 +1,2 @@
-# Load Pesapal config file from YAML file when applicatin is loaded
+# Load Pesapal config file from YAML file when application is loaded
 Pesapal.config = YAML::load(IO.read("#{Rails.root}/config/pesapal.yml"))
